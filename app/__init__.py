0777440d606da885051b73385a7b8292c5852277
davine94 <davine94>
2022-05-10 00:12:52 +0300

    Final changes made on user authentication

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,14 @@ import bcrypt
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5617b591905a7c411a5c7379e4092bc9'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager= LoginManager(app)
 
 from app import views
 
