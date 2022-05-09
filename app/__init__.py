4e6b90d226f00a7cb80d3d4799e8927d60d5680d
davine94 <davine94>
2022-05-09 23:29:30 +0300

    Adding validation properties on the register form

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
+import bcrypt
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5617b591905a7c411a5c7379e4092bc9'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from app import views
 
